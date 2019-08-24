2adaa050b52d1c7495e4a1f50882a2dafd9c859e
intelwalk <intelwalk@gmail.com>
2019-08-23 23:08:07 -0500

    took out keys

--- bottest.py
+++ bottest.py
@@ -6,10 +6,10 @@ import tweepy, time, sys
 argfile = str(sys.argv[1])
 
 #enter the corresponding information from your Twitter application:
-CONSUMER_KEY = 'RhxRePg5AQ5IQuZVrp1K0qPYy'#keep the quotes, replace this with your consumer key
-CONSUMER_SECRET = '34UIfOGkDUPFNZ5iZ5TNLductgeWdLIwJVLDMEqXChy6YkQUqr'#keep the quotes, replace this with your consumer secret key
-ACCESS_KEY = '1060886011-2qfymADaxFoY1l7te2XDJS9wuM2VKucpbzCiJJT'#keep the quotes, replace this with your access token
-ACCESS_SECRET = 'DEGT2saSgxQqXQJuPlVpbwtZYvaIffFZck2FXUqXjoigb'#keep the quotes, replace this with your access token secret
+CONSUMER_KEY = 'xxxxxxxx'#keep the quotes, replace this with your consumer key
+CONSUMER_SECRET = 'xxxxxxxx'#keep the quotes, replace this with your consumer secret key
+ACCESS_KEY = 'xxxxxxxxxxx'#keep the quotes, replace this with your access token
+ACCESS_SECRET = 'xxxxxxxxxx'#keep the quotes, replace this with your access token secret
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
